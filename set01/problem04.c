7c64f5017e616993341c43d9d59d894ea412a1f4
Janaki Raman  Raman <113001884+janakiraman006@users.noreply.github.com>
2022-09-21 04:24:20 +0000

    problem05

---
+++ set01/problem04.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter two number's");
+  scanf("%d",&a);
+  return a;
+}
+int compare(int a,int b)
+{
+  if(a>b)
+    return a;
+  else
+    return b;
+}
+
+  
+int main()
+{
+int x,y,largest;
+x=input();
+y=input();
+largest =compare(x,y);
+printf("the largest of %d,%d is %d\n",x,y, largest);
+return 0;
+  }
---
+++ set01/problem05.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int input()
+{
+  int a
+  prints ("Enter a number");
+  scanf("%d,&a");
+  return a;
+}
+int compare(int a,int b,int c)
+{
+  if (a>b && a>c)
+    return a;
+  else if (b>a && b>c)
+    return b;
+  else
+    return c;
+}
+int main()
+int x,y,z largest;
+x=input();
+y=input();
+z=input();
+largest =compare(x,y,z);
+printf("the largest of %d,%d,%d is %d\n",x,y,z largest);
+return 0;
+  }
