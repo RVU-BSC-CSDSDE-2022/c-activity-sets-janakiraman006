ac34bddc03b64ffe57ee03d9f35511894d8e6d68
Janaki Raman  Raman <113001884+janakiraman006@users.noreply.github.com>
2022-11-22 16:16:43 +0000

    (set 3)Problem 3

--- set03/problem03.c
+++ set03/problem03.c
@@ -1,17 +1,40 @@
-#include<stdio.h>
+#include <studio.h>
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+
+int input_number()
+{
+  int n;
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
+
+int is_composite(int n)
+{
+  for (int i=2;i<n;i++)
+    {
+      if(n %i == 0)
+        return 1
+    }
+    return 0;
+}
+void output(int n, int is_composite)
+{
+  if(is_composite == 0)
+  {
+    printf("%d is not a composite number\n");
+  }
+  else {
+    printf("%d is a composite number\n");
+  }
+}
 int main()
 {
-    int i,n,c=0;
-    printf ("Enter a number n\n");
-    scanf ("%d",&n);
-    for (i=1;i<=n;i++)
-    {
-        if(n%i==0)
-        c=c+1;
-    }
-    if (c==2)
-    printf ("The number is PRIME\n");
-    else
-    printf ("The number is COMPOSITE\n");
-    return 0;
+  int n,isc;
+  n = input();
+  isc = is_composite(n);
+  output(n,isc);
+  return 0;
 }
